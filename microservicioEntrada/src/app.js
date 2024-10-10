966e2860b7c54a00a0c075e9c49d742fcd92a0cc
alelu-pellereca <alejandrapellegrini@gmail.com>
2024-10-10 19:32:50 +0200

    nuevo777

--- microservicioEntrada/src/app.js
+++ microservicioEntrada/src/app.js
@@ -22,7 +22,7 @@ app.get('/microservicio_3', async (req, res) => {
     res.status(200).json({ message: responseAux.message });
 });
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello World from microservicioEntrada 1!' });
+    res.status(200).json({ message: 'Hello World from microservicioEntrada777!' });
 });
 
 // Ruta de ejemplo para pruebas
